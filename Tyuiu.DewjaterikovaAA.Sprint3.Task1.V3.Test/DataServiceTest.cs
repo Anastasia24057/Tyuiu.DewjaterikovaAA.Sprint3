d6bec1dceb3761f1a2ae9e05a04cd8b3315663bb
Anastasia24057 <nastya.dewa@gmail.com>
2025-10-21 21:15:57 +0500

    Task1

---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DewjaterikovaAA.Sprint3.Task1.V3.Lib;
+
+namespace Tyuiu.DewjaterikovaAA.Sprint3.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            double wait = 24589.408;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task1.V3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikovaAA.Sprint3.Task1.V3.Lib;
+DataService ds = new DataService();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startvalue = 1;
+int stopvalue = 10;
+
+
+Console.WriteLine("Старт шага = " + startvalue);
+Console.WriteLine("Старт шага = " + stopvalue);
+
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.Write("Произведение ряда = " + ds.GetMultiplySeries(startvalue, stopvalue));
+Console.ReadKey();
