fd511a0949b79bfe672d35e4162bfa31e514f93d
Anastasia24057 <nastya.dewa@gmail.com>
2025-10-22 19:54:42 +0500

    Task3

---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DewjaterikovaAA.Sprint3.Task3.V4.Lib;
+
+namespace Tyuiu.DewjaterikovaAA.Sprint3.Task3.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void DeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string str = "plkjjdw cvjkl";
+            char chr = 'j';
+            string res = ds.DeleteCharInString(str, chr);
+            string wait = "plk  dw cv kl";
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task3.V4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikovaAA.Sprint3.Task3.V4.Lib;
+
+DataService ds = new DataService();
+string str = "plkjjdw cvjkl";
+char chr = 'j';
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine("Строка =  " + str);
+Console.WriteLine("Удаляемый символ = " + chr);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Конечная строка : " + ds.DeleteCharInString(str, chr));
+Console.ReadKey();
