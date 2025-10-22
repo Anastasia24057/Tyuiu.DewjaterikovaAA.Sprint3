2a89f463791eccf02e9ebbeb360e84c83849f40a
Anastasia24057 <nastya.dewa@gmail.com>
2025-10-22 20:45:57 +0500

    Task4

---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DewjaterikovaAA.Sprint3.Task4.V19.Lib;
+
+namespace Tyuiu.DewjaterikovaAA.Sprint3.Task4.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 9.432;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task4.V19/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikovaAA.Sprint3.Task4.V19.Lib;
+
+DataService ds = new DataService();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Старт шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+Console.ReadKey();
