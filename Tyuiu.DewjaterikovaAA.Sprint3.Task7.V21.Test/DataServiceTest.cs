18274c283fe7062290c782ae8f42521973ac8516
Anastasia24057 <nastya.dewa@gmail.com>
2025-10-23 14:18:19 +0500

    Task7

---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.DewjaterikovaAA.Sprint3.Task7.V21.Lib;
+
+namespace Tyuiu.DewjaterikovaAA.Sprint3.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startvalue = -5;
+            int stopvalue = 5;
+
+            int len = stopvalue - startvalue + 1;
+
+            double[] valueMaitArray;
+            valueMaitArray = new double[len];
+
+            valueMaitArray[0] = 28.05;
+            valueMaitArray[1] = 22.27;
+            valueMaitArray[2] = 17.01;
+            valueMaitArray[3] = 12.35;
+            valueMaitArray[4] = 7.51;
+            valueMaitArray[5] = 0.00;
+            valueMaitArray[6] = 1.62;
+            valueMaitArray[7] = -7.03;
+            valueMaitArray[8] = -12.99;
+            valueMaitArray[9] = -17.55;
+            valueMaitArray[10] = -21.43;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startvalue, stopvalue);
+            CollectionAssert.AreEqual(valueMaitArray, res);
+            
+
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikovaAA.Sprint3.Task7.V21/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikovaAA.Sprint3.Task7.V21.Lib;
+
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startvalue = -5;
+int stopvalue = 5;
+
+
+Console.WriteLine("Начало шага = " + startvalue);
+Console.WriteLine("Конец шага = " + stopvalue);
+
+int len = ds.GetMassFunction(startvalue, stopvalue).Length;
+double[] valueArray;
+valueArray = new double[len];
+
+valueArray = ds.GetMassFunction(startvalue, stopvalue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("+-----------+----------+");
+Console.WriteLine("|    X      |    F(X)  |");
+Console.WriteLine("+-----------+----------+");
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("|{0,4:d}       |{1,7:f2}   |", startvalue, valueArray[i]);
+    startvalue++;
+}
+Console.WriteLine("+-----------+----------+");
+Console.ReadKey();
+
